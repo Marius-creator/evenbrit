988d4338d2d82c71feb2f7c1b583725e3ab44a4c
Marius-creator <marius.martinez40@gmail.com>
2019-11-04 12:53:39 +0100

    initi

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,10 @@ validates :start_date, presence: true #il est impossible de créer ou modifier u
 validates :duration, presence: true #le nombre de minutes doit être un multiple de 5, et est strictement positif
 validates :title, presence: true, length: { in: 5..140 } #doit faire au moins 5 caractères et maxi 140 caractères.
 validates :description, presence: true, length: { in: 20..1000 }
-validates :price, presence: true, numericality: { greater_than: 1 }, numericality: { less_than: 1 }
+validates :price, presence: true#, numericality: { greater_than: 1 }, numericality: { less_than: 1 }
 validates :location, presence: true
 
 belongs_to :user #Un événement appartient à un administrateur (utilisateur)
 has_many :participations #Un événement a plusieurs participations
-has_many :users through :participations #plusieurs participants (utilisateurs) au travers des participations
+has_many :users, through: :participations #plusieurs participants (utilisateurs) au travers des participations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :events #Un utilisateur peut administrer plusieurs événements.
   has_many :events, through: :participations #Un utilisateur peut participer à plusieurs événements au travers des participations.
+
+  after_create :welcome_send
+    def welcome_send
+      UserMailer.welcome_email(self).deliver_now
+    end
 end
